22805e9011e774be44de19d07579a33a04ae3408
Neverhood666 <92528659+Neverhood666@users.noreply.github.com>
2021-10-18 20:08:05 +0300

    math

--- sandbox/src/main/java/test/move/HelloWorld.java
+++ sandbox/src/main/java/test/move/HelloWorld.java
@@ -2,14 +2,26 @@ package test.move;
 
 public class HelloWorld {
     public static void main(String[] args) {
-      System.out.println("Hello world");
+        hello("world");
+        hello("user");
+
+        double len = 5;
+
+        System.out.println(area(len));
 
         System.out.println(2 + 2);
         System.out.println(2 - 2);
         System.out.println(2 * 2);
-        System.out.println(2/2);
-        System.out.println(1.0/2);
+        System.out.println(2 / 2);
+        System.out.println(1.0 / 2);
+    }
+    public static void hello(String somebody) {
+            System.out.println("Hello, " + somebody);
+        }
+
+    public static double area (double l){
+        return l*l;
+    }
 
 
-     }
 }
