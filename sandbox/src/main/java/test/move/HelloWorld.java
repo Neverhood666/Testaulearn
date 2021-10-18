efae3d0251fc57c91c7465bbee6ed61acde64f1b
Neverhood666 <92528659+Neverhood666@users.noreply.github.com>
2021-10-18 19:35:47 +0300

    математика

--- sandbox/src/main/java/test/move/HelloWorld.java
+++ sandbox/src/main/java/test/move/HelloWorld.java
@@ -2,6 +2,14 @@ package test.move;
 
 public class HelloWorld {
     public static void main(String[] args) {
-      System.out.print("Hello world");
+      System.out.println("Hello world");
+
+        System.out.println(2 + 2);
+        System.out.println(2 - 2);
+        System.out.println(2 * 2);
+        System.out.println(2/2);
+        System.out.println(1.0/2);
+
+
      }
 }
