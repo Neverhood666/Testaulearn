7ad6e6ef7b8268af5e0cc2f0dcef233fec1cbdca
Neverhood666 <92528659+Neverhood666@users.noreply.github.com>
2021-11-16 13:08:41 +0300

    Решение первой задачи с проекта Эйлера

---
+++ sandbox/src/main/java/eulerProblems/Problem01.java
@@ -0,0 +1,28 @@
+package eulerProblems;
+
+public class Problem01 {
+    public static void main (String[] args ) {
+
+        int x = 0;
+        int y = 0;
+
+        while (x < 1000) {
+
+            if (x%3==0 || x%5 == 0) {
+                y = y + x;
+
+            }
+            x = x + 1;
+        }
+
+        System.out.println(y);
+
+    }
+}
+
+/*
+Если выписать все натуральные числа меньше 10, кратные 3 или 5, то получим 3, 5, 6 и 9. Сумма этих чисел равна 23.
+Найдите сумму всех чисел меньше 1000, кратных 3 или 5.
+
+Ответ: 233168
+ */
